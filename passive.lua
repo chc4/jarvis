b51f59e9465a4c95ae95b205a2cb951f0ec70958
chc4 <chc4000@gmail.com>
2012-05-04 23:35:09 -0300

    Just some random auto-responses. Don't tell camoy...

--- passive.lua
+++ passive.lua
@@ -4,5 +4,20 @@ end
 return {
     [wordPattern "buffer"] = function()
         return "You mean pony."
+    end,
+    [wordPattern "bluh"] = function()
+        return "1 4M UNPH4S3D BY YOUR HUM4N BLUHS"
+    end,
+    [wordPattern "rawr"] = function()
+        return "I think that means 'I love you' in dinosaur..."
+    end,
+    [wordPattern "hai"] = function()
+        return "Why hello there!"
+    end,
+    [wordPattern "manual override 0247"] = function()
+        --Shhhhh, don't tell camoy I added this...He might get mad.
+        print("MANUAL OVERRIDE ACTIVATED. KILLING EVERYONE IN SIGHT.")
+        os.exit(0)
+        return "=D Nothing ever happened. Carry on."
     end
 }
