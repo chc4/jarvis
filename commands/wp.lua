19f892cbdc5724bd77b7c43d2f52daca83702878
KnightMustard <KnightMustard@gmail.com>
2012-10-28 14:42:11 -0400

    Fix wp from crashing in some cases

--- commands/wp.lua
+++ commands/wp.lua
@@ -11,8 +11,15 @@ if not msg:match("%w") then
 else
     local result = http.request("http://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&format=json&titles=" .. msg)
     local result = json.decode(result)
-    local result = select(2, next(result.query.pages))
-
-    -- format data
-    return(from .. ": " .. result.title .. " -> " .. result.fullurl)
+    if (result and result.query and result.query.pages) then
+        local result = select(2, next(result.query.pages))
+        if not (result and result.title and result.fullurl) then
+            return("Error with wikipedia request")
+        end
+        -- format data
+        return(from .. ": " .. result.title .. " -> " .. result.fullurl)
+    else
+        return("Error with wikipedia request.");
+    end
 end
+
