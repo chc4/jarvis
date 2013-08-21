421497db5cbf8c1b4951ae2c3c6218a37343967e
chc4 <chc4000@gmail.com>
2013-08-21 13:21:23 -0400

    Delete count.lua
    It's useless and was clogging main.lua

--- commands/count.lua
+++
@@ -1,5 +0,0 @@
-local msg, from = ...
-local file = io.open("data/count.json", "r")
-local countData = json.decode(file:read("*a"))
-file:close()
-return ("%s: 0x%08x"):format(from, countData.count)
